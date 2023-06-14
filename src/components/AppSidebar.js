0d834eb109c4c1a4f8d75a456cc48daad4464187
mechevarria <mike.echevarria@gmail.com>
2023-06-14 15:29:43 -0400

    coreui-react import with oi logo edits

---
+++ src/components/AppSidebar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler, CImage } from '@coreui/react'
+
+import { AppSidebarNav } from './AppSidebarNav'
+
+import oiLogoFull from 'src/assets/oi-logo-full.png'
+import oiLogo from 'src/assets/oi-logo.png'
+
+import SimpleBar from 'simplebar-react'
+import 'simplebar/dist/simplebar.min.css'
+
+// sidebar nav config
+import navigation from '../_nav'
+
+const AppSidebar = () => {
+  const dispatch = useDispatch()
+  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
+  const sidebarShow = useSelector((state) => state.sidebarShow)
+
+  return (
+    <CSidebar
+      position="fixed"
+      unfoldable={unfoldable}
+      visible={sidebarShow}
+      onVisibleChange={(visible) => {
+        dispatch({ type: 'set', sidebarShow: visible })
+      }}
+    >
+      <CSidebarBrand className="d-none d-md-flex" to="/">
+        <CImage className="sidebar-brand-full" src={oiLogoFull} height={35} />
+        <CImage className="sidebar-brand-narrow" src={oiLogo} height={35} />
+      </CSidebarBrand>
+      <CSidebarNav>
+        <SimpleBar>
+          <AppSidebarNav items={navigation} />
+        </SimpleBar>
+      </CSidebarNav>
+      <CSidebarToggler
+        className="d-none d-lg-flex"
+        onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+      />
+    </CSidebar>
+  )
+}
+
+export default React.memo(AppSidebar)
